d3665fbb6cb40854dc937df25a35911089e1e39a
carrieyanyi <143552804+carrieyanyi@users.noreply.github.com>
2024-04-05 23:24:07 -0700

    Add bar chart of numerical features comparison among cities in app.py (#41)
    * Add bar chart of numerical features comparison among cities in app.py
    
    * Update dbc.Container error in app.py
    
    ---------
    
    Co-authored-by: Aishwarya Nadimpally <80033106+Aishwarya120111@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # Initialize the Dash app with Bootstrap CSS
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -17,7 +19,6 @@ province_dropdown = dcc.Dropdown(
     options=[{'label': province, 'value': province} for province in sorted(df['Province'].unique())],
     value='British Columbia'  # Changed from sorted(df['Province'].unique())[0]
 )
-
 city_dropdown = dcc.Dropdown(
     id='city-dropdown',
     multi = True
@@ -49,6 +50,18 @@ baths_slider = dcc.Slider(
 output_graph = dcc.Graph(id='output-graph')
 map_graph = dcc.Graph(id='map-graph')
 
+variable1_dropdown = dcc.Dropdown(
+    id='variable1-dropdown',
+    options=[var for var in df[['Price','Number_Beds', 'Number_Baths','Population','Median_Family_Income' ]]],
+    value='Price' 
+)
+variable2_dropdown = dcc.Dropdown(
+    id='variable2-dropdown',
+    options=[var for var in df[['Price','Number_Beds', 'Number_Baths','Population','Median_Family_Income' ]]],
+    value='Median_Family_Income' 
+)
+bar_graph = dcc.Graph(id='bar-graph')
+
 # Assembling global widgets
 global_widgets = [
     dbc.Label("Province"),
@@ -65,16 +78,27 @@ global_widgets = [
     html.Br(),
     dbc.Label("Number of Baths"),
     baths_slider,
+    html.Br(),
+    dbc.Label("Attribute 1"),
+    variable1_dropdown,
+    html.Br(),
+    dbc.Label("Attribute 2"),
+    variable2_dropdown,
+    html.Br()
 ]
 
 # Layout
 app.layout = dbc.Container([
     dbc.Row(dbc.Col(title)),
     dbc.Row([
-        dbc.Col(global_widgets, md=4),
-        dbc.Col(output_graph, md=8),
+
+        dbc.Col(global_widgets, md=2),
+        dbc.Col(bar_graph, md=5),
+         dbc.Col(output_graph,md=5),
         dbc.Col(map_graph,md=8)
-    ])
+        ])
+
+
 ], fluid=True)
 
 # Callback to update city-dropdown options and values based on selected province
@@ -140,6 +164,61 @@ def update_output_graph(province, cities, price_range, beds, baths):
    
     return fig
 
+
+# Callback to update bar graph
+@app.callback(
+    Output('bar-graph', 'figure'),
+    [Input('province-dropdown', 'value'),
+     Input('city-dropdown', 'value'),
+     Input('variable1-dropdown', 'value'),
+     Input('variable2-dropdown', 'value')]
+)
+def grouped_bar_chart(province, cities, var1, var2):
+    if not cities:  # This checks both for None and for an empty list
+        return go.Bar(title="Please select at least one city.")
+    
+    if isinstance(cities, str):
+        cities = [cities]  # Wrap string in list if it's not a list already
+
+    df_province = df[(df['Province'] == province)]
+    df_city = df_province[df_province['City'].isin(cities)]
+    df_var = df_city.groupby('City')[[var1,var2]].mean()
+
+    bar = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    # Add bar 1 for var 1
+    bar.add_trace(go.Bar(
+        x=df_var.index,
+        y=df_var[var1],
+        text=df_var[var1],                # Add the income values as text
+        textposition='auto',              # Position the text automatically
+        texttemplate='%{text:.2s}',
+        name=f'{var1}',
+        marker_color='indianred',
+        offsetgroup=1),
+        secondary_y=False
+    )
+    
+    # Add bar 2 for var 2
+    bar.add_trace(go.Bar(
+        x=df_var.index,
+        y=df_var[var2],
+        text=df_var[var2],  
+        textposition='auto',             
+        texttemplate='%{text:.2s}',
+        name=f'{var2}',
+        marker_color='lightsalmon',
+        offsetgroup=2),
+        secondary_y=True,
+    )
+
+    bar.update_layout(title_text=f'Comparison of {var1} and {var2}',
+                     barmode='group')
+    bar.update_yaxes(title_text=f'{var1}', secondary_y=False)
+    bar.update_yaxes(title_text=f'{var2}', secondary_y=True)
+    
+    return bar
+
 @app.callback(
    
      Output('map-graph', 'figure'),
@@ -181,6 +260,7 @@ def update_map_graph(province, cities):
 
 
 
+
 # Run the Dash application
 if __name__ == '__main__':
-    app.run_server(debug=False, host='127.0.0.1')
+    app.run_server(debug=True, host='127.0.0.1')
