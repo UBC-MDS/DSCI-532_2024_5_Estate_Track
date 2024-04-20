affe41ac7d5614e84d9fa88d240b15d7a9a2d929
Aishwarya Nadimpally <nadimpallyaishwarya@gmail.com>
2024-04-20 13:09:17 -0700

    Updated the data loading from CSV to parquet file and added cache for table

--- src/data.py
+++ src/data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../data/raw/HouseListings.csv', encoding='latin-1')
+df = pd.read_parquet('../data/processed/HouseListings.parquet')
 df.rename(columns={'Number_Beds': 'Bedrooms', 
                     'Number_Baths': 'Bathrooms',
                     'Median_Family_Income': 'Median Family Income'}, inplace=True)
